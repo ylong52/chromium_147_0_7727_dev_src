a4c2f44af1cabe17405e391e5246f92154042a50
ylong52 <ylong52@21cn.com>
2026-07-30 19:22:28 +0800

    add third_party/libvpx

---
+++ third_party/libvpx/source/config/vpx_version.h
@@ -0,0 +1,11 @@
+// This file is generated. Do not edit.
+#ifndef VPX_VERSION_H_
+#define VPX_VERSION_H_
+#define VERSION_MAJOR  1
+#define VERSION_MINOR  16
+#define VERSION_PATCH  0
+#define VERSION_EXTRA  "60-gaec2a6f1c"
+#define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
+#define VERSION_STRING_NOSP "v1.16.0-60-gaec2a6f1c"
+#define VERSION_STRING      " v1.16.0-60-gaec2a6f1c"
+#endif  // VPX_VERSION_H_
